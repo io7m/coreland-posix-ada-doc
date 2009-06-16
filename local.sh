a983f3d03841440bdcde45e452c86e1be9d67f3e
m0 <m0@localhost>
2009-06-16 03:18:11 +0100

    init

---
+++ local.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+SOURCE="${HOME}/git/coreland/posix-ada/"
+
+fatal()
+{
+  echo "fatal: $1" 1>&2
+  exit 1
+}
+
+info()
+{
+  echo "info: $1" 1>&2
+}
+
+#
+# Make procedure map
+#
+
+info "building procedure map"
+
+cat >> src/m_proc_map.ud.tmp <<EOF
+(table procedure_map
+  (t-row (item "POSIX API") (item "Ada equivalent"))
+EOF
+
+(
+for spec in ${SOURCE}/posix*.ads
+do
+  info "processing $spec"
+  ./make-proc_map.lua "$spec" || fatal "could not create proc map for $spec"
+done
+) | sort -k 3 >> src/m_proc_map.ud.tmp
+
+echo ")" >> src/m_proc_map.ud.tmp
+
+mv src/m_proc_map.ud.tmp src/m_proc_map.ud || fatal "could not replace m_proc_map.ud"
