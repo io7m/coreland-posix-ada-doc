ad5cbca439941df7aea3a5d9cb8e96d9fb642783
m0 <m0@localhost>
2009-06-17 01:37:49 +0100

    Z update. More subprograms.

--- src/Z/make-spec.sh
+++ src/Z/make-spec.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
-cat                                \
-z_head.tex                         \
-posix-ada-types.tex.in             \
-posix-file-types-fd.tex.in         \
-posix-file-types-valid_fd.tex.in   \
-posix-file-types-file_name.tex.in  \
-posix-file-types-open_flags.tex.in \
+cat                                      \
+z_head.tex                               \
+posix-ada-types.tex.in                   \
+posix-file-types-fd.tex.in               \
+posix-file-types-valid_fd.tex.in         \
+posix-file-types-file_name.tex.in        \
+posix-file-types-open_access_mode.tex.in \
+posix-file-types-open_options.tex.in     \
 z_foot.tex || exit 1
