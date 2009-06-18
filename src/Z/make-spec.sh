cdcd3a0e72ddd8189b80e3f2be25125e7d12f29e
m0 <m0@localhost>
2009-06-18 16:51:59 +0100

    File_Status.

--- src/Z/make-spec.sh
+++ src/Z/make-spec.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
-cat                                      \
-z_head.tex                               \
-posix-ada-types.tex.in                   \
-posix-path-max_length.tex.in             \
-posix-file-types-fd.tex.in               \
-posix-file-types-valid_fd.tex.in         \
-posix-file-types-file_name.tex.in        \
-posix-file-types-open_access_mode.tex.in \
-posix-file-types-open_options.tex.in     \
+cat                                         \
+z_head.tex                                  \
+posix-ada-types.tex.in                      \
+posix-path-max_length.tex.in                \
+posix-file-types-fd.tex.in                  \
+posix-file-types-valid_fd.tex.in            \
+posix-file-types-file_name.tex.in           \
+posix-file-types-open_access_mode.tex.in    \
+posix-file-types-open_options.tex.in        \
+posix-io-types-storage_element.tex.in       \
+posix-io-types-storage_element_array.tex.in \
 z_foot.tex || exit 1
