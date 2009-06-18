24e1cf4d15e1d70cd2b77df05f9dcd4e7a0c4bfa
m0 <m0@localhost>
2009-06-18 16:54:01 +0100

    Z specifications.

--- src/Z/make-spec.sh
+++ src/Z/make-spec.sh
@@ -9,6 +9,8 @@ posix-file-types-valid_fd.tex.in            \
 posix-file-types-file_name.tex.in           \
 posix-file-types-open_access_mode.tex.in    \
 posix-file-types-open_options.tex.in        \
+posix-file_status-types-device_id.tex.in    \
+posix-file_status-types-inode.tex.in        \
 posix-io-types-storage_element.tex.in       \
 posix-io-types-storage_element_array.tex.in \
 z_foot.tex || exit 1
